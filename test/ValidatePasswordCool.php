2f16b2cfe8578c63ed7a90e51dd4256700244f64
Michael Milawski <michael.milawski@planet-itservices.com>
2016-12-15 13:41:55 +0100

    pushidipush

---
+++ test/ValidatePasswordCool.php
@@ -0,0 +1,18 @@
+<?php
+
+	class ValidatePasswordTest extends PHPUnit_Framework_TestCase{
+
+		public function testValidLength(){
+			$valPass = new ValidatePassword();
+			$this->assertFalse($valPass->validLength('1234'));
+		}
+
+		public function testFail(){
+			$this->assertTrue(true, "nix ist cool!");
+		}
+
+		public function testFailCool2(){
+			$this->assertTrue(false, "Alles Cool");
+		}
+
+	}
